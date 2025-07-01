1dd07754d5b4785d1704067c3a018964480332f7
gvnrxd <208095437+gvnxr@users.noreply.github.com>
2025-06-30 23:54:29 -0400

    Chooses random verse and displays the array

--- random_verse.js
+++ random_verse.js
@@ -1,3 +1,17 @@
-console.log("Hello World");
-let a = "Black";
-console.log(a);
+const welcome_message = "The Bible Verse of the Day is: ";
+//When adding a Bible Verse Make Sure First Word is Chapter!!
+const bible_verses = [`Genesis 1:1 "In the beginning God created the heaven and the earth"`,"Revelations 5:10","John 2:1"];
+const random_FirstWord = () => {
+    const i = Math.floor(Math.random() * bible_verses.length);
+    const chosenVerse = bible_verses[i];
+    const chosenVerseChapter = bible_verses[i].split(" ")[0];
+    return {chosenVerse,
+            chosenVerseChapter};
+};
+
+const result = random_FirstWord();
+
+console.log(`${welcome_message} ${result.chosenVerse}`);
+console.log(result.chosenVerseChapter);
+
+
